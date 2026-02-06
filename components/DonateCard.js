fdea901075dd4b61fc48bd30671ba8d60503966a
st-jliu <33519628+st-jliu@users.noreply.github.com>
2026-02-06 14:55:54 +0800

    Update ad card

--- components/DonateCard.js
+++ components/DonateCard.js
@@ -5,10 +5,10 @@ import { RiPaypalFill } from "react-icons/ri";
 const DonateCard = ({ addMargin = "both" }) => {
     return (
         <div className={`${styles.container} ${addMargin === "both" ? styles.margin : (addMargin === "top" ? styles.marginTop : null)}`}>
-            <h2>Seamless Intune Integration, Effortless App Management with Pckgr</h2>
-            <p><a href="https://intunepckgr.com/?navigation=winstall" rel="sponsored">Pckgr</a> simplifies your Intune application management. Utilizing winget, it streamlines software deployment with easy, one-click processes and automatic updates. Experience a user-friendly, secure way to manage apps across your enterprise. Start your 30 day free trial today!</p>
+            <h2>Deploy Once, Update Forever with Splashtop</h2>
+            <p>Splashtop AEM automates your entire application lifecycle —from vendor and custom packaging to seamless continuous updates —saving time and reduce vulnerability risks.</p>
             <div className={styles.buttons}>
-                <a className="button spacer accent donate" id="starWine" href="https://intunepckgr.com/?navigation=winstall" rel="sponsored"><FiPlus /> Try for Free</a>
+                <a className="button spacer accent donate" id="starWine" href="https://www.splashtop.com/products/autonomous-endpoint-management" rel="sponsored"><FiPlus /> Get Started</a>
             </div>
         </div>
     )
