03190190e59ec7d61b5e4d9b80e10105a0738c82
st-jliu <33519628+st-jliu@users.noreply.github.com>
2026-02-09 14:51:56 +0800

    Update google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -11,8 +11,27 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <script src="https://cmp.osano.com/169nu5TMub7Nk2Epy/7c82a576-285f-4b1c-8d4b-a1a4da5b7346/osano.js"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-5JF42TD');
+              `,
+            }}
+          />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5JF42TD"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
@@ -21,21 +40,6 @@ class MyDocument extends Document {
         <link
           href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600&display=swap"
           rel="stylesheet"
-        />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=UA-48569869-4"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag() { dataLayer.push(arguments); }
-                gtag('js', new Date());
-
-                gtag('config', 'UA-48569869-4');
-              `,
-          }}
         />
       </Html>
     );
