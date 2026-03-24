e0a4f84df577c202d298daa9c807ca3a49d07964
st-jliu <33519628+st-jliu@users.noreply.github.com>
2026-03-24 14:37:55 +0800

    Use serwist instead legacy next-pwa, for support nextjs 15

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
+const withSerwist = require("@serwist/next").default({
+  swSrc: "sw/sw.ts",
+  swDest: "public/sw.js",
   disable: process.env.NODE_ENV === "development",
+  cacheOnNavigation: true,
+  reloadOnOnline: true,
 });
 
-module.exports = withPWA({
+module.exports = withSerwist({
   // Enable standalone mode for Docker builds only
   ...(process.env.STANDALONE_BUILD === "true" && { output: "standalone" }),
 });
